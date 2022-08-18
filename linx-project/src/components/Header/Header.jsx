10e441fb2048b884477762c08d3f50a44476c25e
est8r <estermessias6@gmail.com>
2022-08-17 21:35:58 -0300

    Adicionando tags de titulo no componente Header

--- linx-project/src/components/Header/Header.jsx
+++ linx-project/src/components/Header/Header.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 const Header = () => {
-  return <header></header>;
+  return (
+    <header>
+      <h2>uma seleção de produtos</h2>
+      <h1>especial para voce</h1>
+      <p>
+        Todos os produtos desta lista foram selecionados a partir da sua
+        navegação. Aproveite!
+      </p>
+    </header>
+  );
 };
 
 export default Header;
