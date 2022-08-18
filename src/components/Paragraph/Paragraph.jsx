8c932a7deeaa6ef65ee62d90e48f29796a48c641
est8r <estermessias6@gmail.com>
2022-08-18 17:49:34 -0300

    Iniciando criação do componente paragrafo com titulo

---
+++ src/components/Paragraph/Paragraph.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Paragraph = () => {
+  return (
+    <div>
+      <h2>Ajude o algoritmo a ser mais certeiro</h2>
+    </div>
+  );
+};
+
+export default Paragraph;
