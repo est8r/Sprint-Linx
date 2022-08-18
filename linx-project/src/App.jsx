d0b2e31daeb3d9116d9410cbbcc1d33c6af25108
est8r <estermessias6@gmail.com>
2022-08-17 21:31:21 -0300

    Importanto componente header no App principal

--- linx-project/src/App.jsx
+++ linx-project/src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from 'react';
+import Header from './components/Header/Header.jsx';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
