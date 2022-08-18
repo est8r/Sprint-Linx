9b9a409e1b9e03632f16688b8eafad37c18e23c5
est8r <estermessias6@gmail.com>
2022-08-17 21:37:09 -0300

    Importanto classes de estilo nas tags do componente

--- linx-project/src/components/Header/Header.jsx
+++ linx-project/src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import S from './Header.module.css';
 const Header = () => {
   return (
-    <header>
-      <h2>uma seleção de produtos</h2>
-      <h1>especial para voce</h1>
+    <header className={S.header}>
+      <h2 className={S.h2}>uma seleção de produtos</h2>
+      <h1 className={S.h1}>especial para voce</h1>
       <p>
         Todos os produtos desta lista foram selecionados a partir da sua
         navegação. Aproveite!
