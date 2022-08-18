71e2f023fba260b1c3ade899cd7d3bce62218bd5
est8r <estermessias6@gmail.com>
2022-08-18 17:56:45 -0300

    Importando componente Paragrafo dentro da Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Paragraph from '../Paragraph/Paragraph.jsx';
 import S from './Main.module.css';
 
 const Main = () => {
-  return <main className={S.main}></main>;
+  return (
+    <main className={S.main}>
+      <Paragraph />
+    </main>
+  );
 };
 
 export default Main;
