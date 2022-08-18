109e6811d82e7505a53417f11cde58d4f8a72aa1
est8r <estermessias6@gmail.com>
2022-08-18 17:44:06 -0300

    Criação do menu principal na header

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import S from './Menu.module.css';
+const Menu = () => {
+  return (
+    <div>
+      <nav>
+        <button>Conheça a Linx</button>
+        <button>Ajude o algoritmo</button>
+        <button>Seus Produtos</button>
+        <button>Compartilhe</button>
+      </nav>
+    </div>
+  );
+};
+
+export default Menu;
