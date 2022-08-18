5c4a9589eae2e8ceb85f21998a960f6181b4e150
est8r <estermessias6@gmail.com>
2022-08-18 17:42:15 -0300

    Adicionando o estilo criado no paragrafo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className={S.header}>
       <h2 className={S.h2}>uma seleção de produtos</h2>
       <h1 className={S.h1}>especial para voce</h1>
-      <p>
+      <p className={S.p}>
         Todos os produtos desta lista foram selecionados a partir da sua
         navegação. Aproveite!
       </p>
