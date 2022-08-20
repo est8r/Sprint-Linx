eb85acb8dccd058090d2c61749d57e1c3ec9c82d
est8r <estermessias6@gmail.com>
2022-08-19 23:21:04 -0300

    Importando o componente Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import Header from './components/Header/Header.jsx';
+import Main from './components/Main/Main.jsx';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
