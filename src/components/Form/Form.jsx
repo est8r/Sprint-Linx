ddf16ab0aee98023e9b42c16894643d8be261f9f
est8r <estermessias6@gmail.com>
2022-08-19 23:10:18 -0300

    Criação dos inputs de tipo texto, e-mail e radio

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,7 +2,33 @@ import React from 'react';
 import Label from '../Label/Label.jsx';
 import Input from '../Input/Input';
 const Form = () => {
-  return <form></form>;
+  return (
+    <form>
+      <div>
+        <Label texto="Seu Nome" />
+        <Input id="name" type="text" />
+      </div>
+      <div>
+        <Label texto="Email" />
+        <Input id="email" type="email" />
+      </div>
+      <div>
+        <Label texto="CPF" />
+        <Input id="cpf" type="text" />
+      </div>
+
+      <section>
+        <div>
+          <Input id="fem" name="radio" type="radio" />
+          <Label texto="Feminino" />
+        </div>
+        <div>
+          <Input id="masc" name="radio" type="radio" />
+          <Label texto="Masculino" />
+        </div>
+      </section>
+    </form>
+  );
 };
 
 export default Form;
