8edfd8646e6f7935c7d2fbcef50004296589b98f
est8r <estermessias6@gmail.com>
2022-08-19 23:16:01 -0300

    Criando novo botão e span na header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import Menu from '../Menu/Menu';
 import S from './Header.module.css';
+import Button from '../Button/Button.jsx';
+import Menu from '../Menu/Menu';
+
 const Header = () => {
   return (
     <header className={S.header}>
+      <Menu />
       <h2 className={S.h2}>uma seleção de produtos</h2>
-      <h1 className={S.h1}>especial para voce</h1>
+      <h1 className={S.h1}>
+        especial para <span className={S.diff}>você</span>
+      </h1>
       <p className={S.p}>
         Todos os produtos desta lista foram selecionados a partir da sua
-        navegação. Aproveite!
+        navegação.
       </p>
-      <Menu />
+      <div className={S.div}>
+        <span className={S.span}>Como fazemos isso?</span>
+        <Button className={S.button} content={'Saiba Mais'} />
+      </div>
     </header>
   );
 };
