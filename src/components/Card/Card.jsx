e8529cca4ca4266013a165a834ccd15ffe568ddd
est8r <estermessias6@gmail.com>
2022-08-20 23:38:12 -0300

    Criando codigo base para o componente Card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Card = ({ img, name, description, oldValue, newValue, times, or }) => {
+  return (
+    <div>
+      <div>
+        <img src={img} alt="product" />
+      </div>
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <p>{'R$ ' + oldValue + ',00'}</p>
+      <h4>{'R$ ' + newValue + ',00'}</h4>
+      <p>
+        ou {times + 'x'} de: {'R$ ' + or}
+      </p>
+    </div>
+  );
+};
+
+export default Card;
