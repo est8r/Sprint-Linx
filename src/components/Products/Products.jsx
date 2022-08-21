add18506c75bcf0e4826a4af67352c5fdc491cfa
est8r <estermessias6@gmail.com>
2022-08-21 00:12:31 -0300

    iniciando criação da requisição da API com useState

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,9 +3,19 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 
 const Products = () => {
+  const [card, setNewCard] = useState([]);
+  const [section, setSection] = useState(1);
+
+  const handleReq = async () => {
+    const url = `https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1${section}`;
+    const response = await fetch(url);
+    const json = await response.json();
+    setNewCard([...card, ...json.products]);
+  };
+
   return (
     <div>
-      <div></div>
+      <div>); })}</div>
     </div>
   );
 };
