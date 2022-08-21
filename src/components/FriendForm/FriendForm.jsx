60d77923c215e7fdb5074e2fb5a957ff5360a31a
est8r <estermessias6@gmail.com>
2022-08-21 00:20:35 -0300

    Importando estilizações

--- src/components/FriendForm/FriendForm.jsx
+++ src/components/FriendForm/FriendForm.jsx
@@ -2,25 +2,26 @@ import React from 'react';
 import Input from '../Input/Input.jsx';
 import Label from '../Label/Label.jsx';
 import Button from '../Button/Button.jsx';
+import S from './FriendForm.module.css';
 
 const FriendForm = () => {
   return (
-    <form>
-      <p>
+    <form className={S.form}>
+      <p className={S.p}>
         Quer que seus amigos também ganhem a lista personalizada deles? Preencha
         agora!
       </p>
-      <section>
-        <div>
+      <section className={S.section}>
+        <div className={S.divInput}>
           <Label texto="Nome do amigo:" />
           <Input type="text" />
         </div>
-        <div>
+        <div className={S.divInput}>
           <Label texto="E-mail do amigo:" />
           <Input type="email" />
         </div>
       </section>
-      <Button content="Enviar" />
+      <Button className={S.button} content="Enviar" />
     </form>
   );
 };
