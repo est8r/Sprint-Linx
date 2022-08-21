a08405d0efeccc196182db18068c82fa05169219
est8r <estermessias6@gmail.com>
2022-08-20 23:30:11 -0300

    Adicionando onClick como props

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Button = ({ content, className }) => {
-  return <button className={className}>{content}</button>;
+const Button = ({ onClick, content, className }) => {
+  return (
+    <button onClick={onClick} className={className}>
+      {content}
+    </button>
+  );
 };
 
 export default Button;
