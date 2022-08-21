52c8a1db2d24dc61aad19c08664d068695912e0a
est8r <estermessias6@gmail.com>
2022-08-21 00:19:15 -0300

    Criando componente do segundo Formulario

---
+++ src/components/FriendForm/FriendForm.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const FriendForm = () => {
+  return (
+    <form>
+      <p>
+        Quer que seus amigos também ganhem a lista personalizada deles? Preencha
+        agora!
+      </p>
+      <section>
+        <div></div>
+        <div></div>
+      </section>
+    </form>
+  );
+};
+
+export default FriendForm;
