b2ababa35ad46cc05ee36e3263911ca72677fbd2
est8r <estermessias6@gmail.com>
2022-08-21 00:07:20 -0300

    Criando código base para componente Products

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+const Products = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Products;
