8f61acaa3de28dff15976ce73a08daf14ed0495b
est8r <estermessias6@gmail.com>
2022-08-22 17:33:50 -0300

    função para validar inputs criada utilizando os objetos

--- src/components/Form/useForm.jsx
+++ src/components/Form/useForm.jsx
@@ -17,6 +17,19 @@ const useForm = (type) => {
   const [value, setValue] = useState('');
   const [error, setError] = useState(null);
 
+  function validate(value) {
+    if (type === false) return true;
+    if (value.length === 0) {
+      setError('Preencha um valor');
+      return false;
+    } else if (types[type] && !types[type].regex.test(value)) {
+      setError(types[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
 
 
   return {
