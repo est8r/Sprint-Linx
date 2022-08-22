05f326f3d47d615b0755fadcff1881cfcb60655b
est8r <estermessias6@gmail.com>
2022-08-22 17:24:03 -0300

    Adicionando algumas props novas

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import S from './Input.module.css';
 
-const Input = ({ id, type }) => {
-  return <input className={S.input} id={id} type={type} />;
+const Input = ({ id, type, value, error, onChange, onBlur, placeholder }) => {
+  return (
+    <>
+      <input
+        className={S.input}
+        type={type}
+        id={id}
+        name={id}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        placeholder={placeholder}
+      />
+      {error && <p className={S.p}>{error}</p>}
+    </>
+  );
 };
 
 export default Input;
