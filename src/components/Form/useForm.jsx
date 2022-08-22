afe9785bb279a3912bdc0809d2058d521d969463
est8r <estermessias6@gmail.com>
2022-08-22 17:28:50 -0300

    documento useForm criado com regex para e-mail

---
+++ src/components/Form/useForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useState } from 'react';
+
+const types = {
+  email: {
+    regex:
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: 'Email inválido',
+  },
+};
+
+return {};
+
+export default useForm;
