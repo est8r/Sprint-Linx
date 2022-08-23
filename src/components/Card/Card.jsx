270ced87867ed371d7ef35eb3fd202302599af19
est8r <estermessias6@gmail.com>
2022-08-22 22:53:30 -0300

    fix: sintaxe dos componentes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,18 +6,20 @@ import S from './Card.module.css';
 const Card = ({ img, name, description, oldValue, newValue, times, or }) => {
   return (
     <div className={S.div}>
-      <Rating />
       <div className={S.img}>
+        <Rating />
         <img className={S.img} src={img} alt="produto" />
       </div>
-      <h2 className={S.h2}>{name}</h2>
-      <p className={S.desc}>{description}</p>
-      <p className={S.oldValue}>{'R$ ' + oldValue + ',00'}</p>
-      <h4 className={S.h4}>{'R$ ' + newValue + ',00'}</h4>
-      <p className={S.p}>
-        ou {times + 'x'} de: {'R$ ' + or}
-      </p>
-      <Button className={S.button} content="Comprar" />
+      <div className={S.content}>
+        <h2 className={S.h2}>{name}</h2>
+        <p className={S.desc}>{description}</p>
+        <p className={S.oldValue}>{'R$ ' + oldValue + ',00'}</p>
+        <h4 className={S.h4}>{'R$ ' + newValue + ',00'}</h4>
+        <p className={S.p}>
+          ou {times + 'x'} de: {'R$ ' + or}
+        </p>
+        <Button className={S.button} content="Comprar" />
+      </div>
     </div>
   );
 };
